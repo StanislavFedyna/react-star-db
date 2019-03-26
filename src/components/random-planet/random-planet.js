05cea73680714058536078ff11cad95f2144f691
StanislavFedyna <stanislavfedyna@gmail.com>
2019-03-26 16:36:33 +0200

    added catch function for updatePlanet

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -4,6 +4,7 @@ import SwapiService from '../../services/swapi-service';
 
 import './random-planet.css';
 import Spinner from '../spinner';
+import ErrorIndicator from '../error-indicator'
 
 export default class RandomPlanet extends Component {
 
@@ -11,7 +12,8 @@ export default class RandomPlanet extends Component {
 
   state = {
     planet: {},
-    loading: true
+    loading: true,
+    error: false
   };
 
   constructor() {
@@ -25,23 +27,33 @@ export default class RandomPlanet extends Component {
       loading: false  
     });
   };
-
+  onError = ()  => {
+    this.setState({
+      error: true,
+      loading: false
+    })
+  }
   updatePlanet() {
-    const id = Math.floor(Math.random()*13)+1
+    const id = Math.floor(Math.random()*13)+1;
     this.swapiService
       .getPlanet(id)
-      .then(this.onPlanetLoaded);
+      .then(this.onPlanetLoaded)
+      .catch(this.onError);
   }
 
   render() {
 
-    const { planet, loading } = this.state;
+    const { planet, loading, error } = this.state;
 
+    const hasData =!(loading || error);
+
+    const errorMessage = error ? <ErrorIndicator/> : null
     const spinner = loading ? <Spinner/> : null;
-    const content = !loading ? <PlanetView planet={planet}/> : null;
+    const content = hasData ? <PlanetView planet={planet}/> : null;
 
     return (
       <div className="random-planet jumbotron rounded">
+        {errorMessage}
         {spinner}
         {content}
       </div>
@@ -52,6 +64,7 @@ export default class RandomPlanet extends Component {
 
 const PlanetView = ({ planet }) => {
   const { id, name, population, rotationPeriod, diameter } = planet;
+
 
   return(
     <React.Fragment>
@@ -77,3 +90,4 @@ const PlanetView = ({ planet }) => {
     </React.Fragment>
   )
 }
+
